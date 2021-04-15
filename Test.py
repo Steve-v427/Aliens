fc31067ac7d6dd9dfd469d68f4bb1a87869d8d13
Steve-v427 <82559318+Steve-v427@users.noreply.github.com>
2021-04-14 23:43:59 -0400

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,53 @@
+import unittest
+
+from alien_invasion import AlienInvasion
+from settings import Settings
+from scoreboard import Scoreboard
+
+class TestSettings(unittest.TestCase):
+    def setUp(self):
+        self.maxspeedup_scale = Settings()
+
+    def test_speedup_scale(self):
+        self.assertNotEqual(self.maxspeedup_scale.speedup_scale, 2)
+        if True:
+            print("Test 1 Sucess!")
+
+class TestSetting(unittest.TestCase):
+    def setUp(self):
+        self.starting_ship_speed = Settings()
+
+    def test_starting_ship_speed(self):
+        self.assertNotEqual(self.starting_ship_speed.ship_speed, 6)
+        if True:
+            print("Test 2 Sucess!")
+
+class TestBulletAmmount(unittest.TestCase):
+    def setUp(self):
+        self.maxbulletamount = Settings()
+
+    def test_bullet_amount(self):
+        self.assertNotEqual(self.maxbulletamount.bullets_allowed, 11)
+        if True:
+            print("Test 3 Sucess!")
+
+class TestBulletColor(unittest.TestCase):
+    def setUp(self):
+        self.wrongbulletcolor = Settings()
+
+    def test_bullet_colort(self):
+        self.assertNotEqual(self.wrongbulletcolor.bullet_color, (211, 211, 211))
+        if True:
+            print("Test 4 Sucess!")
+
+class TestNumberofLives(unittest.TestCase):
+    def setUp(self):
+        self.maxamountoflives = Settings()
+
+    def test_bullet_colort(self):
+        self.assertNotEqual(self.maxamountoflives.ship_limit, 4)
+        if True:
+            print("Test 5 Sucess!")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ settings.py
@@ -0,0 +1,59 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+        #self.background = pygame.image.load('Alieninvadors/images/background.jpg')
+        self.ship_limit = 3
+        # Maximum amount of lives is 3!
+        if self.ship_limit > 3:
+            raise Exception("Maximum amount of ships reached")
+
+        # Bullet settings
+        self.bullet_speed = 2
+        self.bullet_width = 8
+        self.bullet_height = 17
+        self.bullet_color = (255, 255, 255)
+        self.bullets_allowed = 10
+        if self.bullets_allowed >= 11:
+            raise Exception("Maximum amount of Bullets reached!")
+
+        # Alien settings
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.3
+        if self.speedup_scale >= 1.8:
+            raise Exception("Speedup_Scale Too High")
+
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Settings that change throughout the game."""
+        self.ship_speed = 4
+        # Maximum ship starting speed is 5
+        if self.ship_speed > 5:
+            raise Exception("Starting Speed too High!")
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
